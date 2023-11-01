80247694a2cb3e34b03e5b891ff0109c62c1b1cb
Fernando Lopez <ingfglopez@gmail.com>
2023-11-01 17:08:29 -0300

    Base con footer y header

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const path = require('path');
 
 const app = express();
@@ -16,3 +16,35 @@ app.get('/', (req, res) => {
 app.get('/iniciar-sesion', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/login.html'))
 })
+*/
+
+const express = require("express");
+
+const path = require("node:path");
+const server = express();
+server.use(express.static("public"));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, err => {
+  console.log(
+    err
+      ? `Servidor no pudo montarse. ${err}`
+      : `Servidor corre en http://localhost:${PORT}`
+  );
+});
+
+server.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/index.html"));
+});
+
+
+server.get("/quienesomos", (req, res) => {
+  res.sendFile(path.join(__dirname, "./views/quienesomos.html"));
+});
+
+
+server.get("/about", (req, res) => {
+  res.json({
+    message: "Hola ",
+    aboutUs: "Grupo 6",
+  });
+});
